12ce83a159b70c0307272b1235bf2767996f83a9
GerniVisser-bbd <Gerrit.Visser@bbd.co.za>
2024-07-25 15:31:38 +0200

    streaming

--- backend/MainEvent/MainEventBackend/Hubs/ChatHub.cs
+++ backend/MainEvent/MainEventBackend/Hubs/ChatHub.cs
@@ -1,10 +1,12 @@
 ﻿using Confluent.Kafka;
 using MainEvent.DTO;
+using MainEvent.helpers;
 using MainEvent.Helpers;
 using MainEvent.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MainEvent.Hubs
 {
@@ -19,8 +21,28 @@ namespace MainEvent.Hubs
         [Authorize]
         public override async Task OnConnectedAsync()
         {
-            
             await Clients.Caller.SendAsync("StartMessage", _topic);
+
+            var earliestConfig = new ConsumerConfig
+            {
+                BootstrapServers = "54.154.112.105:29092",
+                GroupId = Guid.NewGuid().ToString(),
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+            };
+            var consumer = new ConsumerBuilder<Null, MessageData>(earliestConfig)
+                .SetValueDeserializer(new JsonSerializable<MessageData>())
+                .Build();
+            consumer.Subscribe(_topic.topic);
+            var flag = false;
+            while (true)
+            {
+                var result = consumer.Consume(5000);
+                if (result == null && flag) break;
+                if (result == null) continue;
+                flag = true;
+                await Clients.Caller.SendAsync("ReceiveMessage", result.Message.Value);
+            }
+
             await base.OnConnectedAsync();
 
         }
