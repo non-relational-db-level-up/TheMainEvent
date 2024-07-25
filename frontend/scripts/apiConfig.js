60d7a5b253dfa0f5a0c27fdb89a9c6357f88d70f
GerniVisser-bbd <Gerrit.Visser@bbd.co.za>
2024-07-25 11:12:48 +0200

    api Config changes

--- frontend/scripts/apiConfig.js
+++ frontend/scripts/apiConfig.js
@@ -5,7 +5,7 @@ let backendUrl = 'https://api.themainevent.projects.bbdgrad.com';
 let redirectUri = 'https://themainevent.projects.bbdgrad.com/views/callback.html';
 let logoutRedirectUri = 'https://themainevent.projects.bbdgrad.com/views/logout.html';
 if (hostname.includes("localhost") || hostname.includes("127.0.0.1")) {
-  backendUrl = 'https://localhost:8080';
+  backendUrl = 'https://localhost:5000';
   redirectUri = 'http://localhost:5500/views/callback.html';
   logoutRedirectUri = 'http://localhost:5500/views/logout.html';
 }
